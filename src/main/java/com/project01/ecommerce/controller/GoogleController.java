aaaaad51b80e82976b843c507b0f932da98dcfbc
tran quoc dat <155450566+Trandatdev89@users.noreply.github.com>
2024-11-13 23:51:06 +0700

    update v2

--- src/main/java/com/project01/ecommerce/controller/GoogleController.java
+++ src/main/java/com/project01/ecommerce/controller/GoogleController.java
@@ -1,12 +1,6 @@
 //package com.project01.ecommerce.controller;
 //
-//import com.project01.ecommerce.model.response.ApiResponse;
-//import com.project01.ecommerce.model.response.AuthenticateResponse;
-//import com.project01.ecommerce.services.AuthServices;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-//import org.springframework.web.bind.annotation.GetMapping;
+//
 //import org.springframework.web.bind.annotation.RestController;
 //
 //import java.io.IOException;
@@ -14,18 +8,5 @@
 //@RestController
 //public class GoogleController {
 //
-//    @Autowired
-//    private AuthServices authServices;
 //
-//
-//    @GetMapping("/login/oauth2/code/google")
-//    public ApiResponse<AuthenticateResponse> user(OAuth2AuthenticationToken oAuth2AuthenticationToken,HttpServletResponse response) throws IOException {
-//
-//        response.sendRedirect("http://localhost:3000/chatapp");
-//        return ApiResponse.<AuthenticateResponse>builder()
-//                .code(200)
-//                .message("success!")
-//                .data(authServices.loginWithGoogle(oAuth2AuthenticationToken))
-//                .build();
-//    }
 //}
